5af9761be54653ca12c80d718e6441006cf34b5e
welleh <welleh10@gmail.com>
2020-01-08 00:28:14 -0300

    add sql

--- chat.sql
+++ chat.sql
@@ -1,9 +1,8 @@
 /*
 SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.6.41-log : Database - chat
+MySQL - 5.7.21-21 : Database - ifcombr_teste
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -13,26 +12,26 @@ MySQL - 5.6.41-log : Database - chat
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`chat` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ifcombr_teste` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
-USE `chat`;
+USE `ifcombr_teste`;
 
 /*Table structure for table `ci_session` */
 
 DROP TABLE IF EXISTS `ci_session`;
 
 CREATE TABLE `ci_session` (
-  `id` varchar(128) NOT NULL,
-  `ip_address` varchar(45) NOT NULL,
-  `user_agent` varchar(120) NOT NULL,
+  `id` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `ip_address` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `user_agent` varchar(120) COLLATE utf8_unicode_ci NOT NULL,
   `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
   `data` blob NOT NULL,
   KEY `ci_sessions_timestamp` (`timestamp`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `ci_session` */
 
-insert  into `ci_session`(`id`,`ip_address`,`user_agent`,`timestamp`,`data`) values ('rpaipd6ljak2n64rndb2m4qalfam4tfk','::1','',1578082504,'__ci_last_regenerate|i:1578082504;sessao_user|a:1:{i:0;a:6:{s:2:\"id\";s:1:\"1\";s:4:\"nome\";s:17:\"Wellisson Ribeiro\";s:5:\"email\";s:18:\"welleh10@gmail.com\";s:5:\"login\";s:9:\"wellisson\";s:5:\"senha\";s:32:\"e803adae1f5acc155699ad43e9b77629\";s:6:\"inicio\";s:1:\"0\";}}'),('evf1lipfdf3o4ungho910t43lllepsfv','::1','',1578083283,'__ci_last_regenerate|i:1578083283;sessao_user|a:1:{i:0;a:6:{s:2:\"id\";s:1:\"1\";s:4:\"nome\";s:17:\"Wellisson Ribeiro\";s:5:\"email\";s:18:\"welleh10@gmail.com\";s:5:\"login\";s:9:\"wellisson\";s:5:\"senha\";s:32:\"e803adae1f5acc155699ad43e9b77629\";s:6:\"inicio\";s:1:\"0\";}}'),('c2e1pdjvrhggk8nhb3u3qn9n3e8bvi8e','::1','',1578083687,'__ci_last_regenerate|i:1578083283;sessao_user|a:1:{i:0;a:6:{s:2:\"id\";s:1:\"1\";s:4:\"nome\";s:17:\"Wellisson Ribeiro\";s:5:\"email\";s:18:\"welleh10@gmail.com\";s:5:\"login\";s:9:\"wellisson\";s:5:\"senha\";s:32:\"e803adae1f5acc155699ad43e9b77629\";s:6:\"inicio\";s:1:\"0\";}}');
+insert  into `ci_session`(`id`,`ip_address`,`user_agent`,`timestamp`,`data`) values ('easi7o2h0j6v4u5qlk4hs6dknr1iokl0','::1','',1578450047,'__ci_last_regenerate|i:1578450047;'),('304f74284763fe072e1cbbdb9d1af4413d54608b','200.53.22.242','',1578450902,'__ci_last_regenerate|i:1578450902;'),('d595621bcde9248947e39883676106d51e78bda5','200.53.22.242','',1578450841,'__ci_last_regenerate|i:1578450841;FBRLH_state|s:32:\"cb2dd1daab1599677c9c5e195d3ddad0\";'),('185d5447e4e50086d190f068929269d5b73ed44b','200.53.22.242','',1578450843,'__ci_last_regenerate|i:1578450843;FBRLH_state|s:32:\"3dd30d7d50050736fedb1d69fe0c3c7f\";'),('ar0s7scgcgokq0cjj5di4fam1s9i95qj','::1','',1578451399,'__ci_last_regenerate|i:1578451399;FBRLH_state|s:32:\"ffdb7d0fd51430f97d04e0f604d2635e\";'),('f7b75e0419a9d24f3f53c3ca74d9cf78d880cd82','200.53.22.242','',1578450184,'__ci_last_regenerate|i:1578450184;'),('3b70d705192838789b8638cfeabc428a36eaa646','69.171.251.23','',1578450398,'__ci_last_regenerate|i:1578450398;'),('4370a43ce193542486ae89837cab000be03ebc72','173.252.95.26','',1578450568,'__ci_last_regenerate|i:1578450568;FBRLH_state|s:32:\"6be8196655dde6ba687c06eb500f2264\";'),('da1b008558df8d3fbe39bb87801b486f21344f15','200.53.22.242','',1578451717,'__ci_last_regenerate|i:1578451717;FBRLH_state|s:32:\"cb2dd1daab1599677c9c5e195d3ddad0\";'),('a1b9ffeb0572206509c2ff7d376a11ce103fe4c1','200.53.22.242','',1578451226,'__ci_last_regenerate|i:1578450692;FBRLH_state|s:32:\"e3d446df8b6f34cd62b4d4219b5f34b4\";'),('194eabc9309efea874e51b495842fefd7e24a5d6','200.53.22.242','',1578451735,'__ci_last_regenerate|i:1578451735;FBRLH_state|s:32:\"3dd30d7d50050736fedb1d69fe0c3c7f\";'),('8f1d499094024287c3790271a9d9cdd7a506e23f','200.53.22.242','',1578451225,'__ci_last_regenerate|i:1578450902;'),('ofgpl3bt41a04v6ue0vs8i3qvpnvn88a','::1','',1578451401,'__ci_last_regenerate|i:1578451399;FBRLH_state|s:32:\"ffdb7d0fd51430f97d04e0f604d2635e\";'),('kk1lppejodgvhs4n8qgoaugtn1dp3br7','::1','',1578452619,'__ci_last_regenerate|i:1578452619;FBRLH_state|s:32:\"37eee4bc5f1f4958b38bea197af104b8\";face_access_token|s:183:\"EAAKm1AHvs8UBANGMoLJAmZAmQihZBEQPXdwVeFEIZAaHtqFhOPqaL4epOWVZBQb1eUBmZBOdlVl7MfC4ZCSZBSYdFayy27izzOGPlqWZAANJZCJ5vbJqa6Y0lnmZB5UpPWDCTTkkJfNrroqshp4rNSZCqv4kmHKGpn6nacqohVpDZAE7kQZDZD\";'),('978c66bc8b4eba648561f2b261152a8c9b7888ab','200.53.22.242','',1578452550,'__ci_last_regenerate|i:1578452550;FBRLH_state|s:32:\"3dd30d7d50050736fedb1d69fe0c3c7f\";sessao_user|a:1:{i:0;a:7:{s:2:\"id\";s:1:\"1\";s:4:\"nome\";s:17:\"Wellisson Ribeiro\";s:5:\"email\";s:18:\"welleh10@gmail.com\";s:5:\"login\";s:9:\"wellisson\";s:5:\"senha\";s:32:\"e803adae1f5acc155699ad43e9b77629\";s:6:\"inicio\";s:10:\"1578452454\";s:6:\"imagem\";s:135:\"https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2640547489397903&height=50&width=50&ext=1581043405&hash=AeQq-hT7NG1Ykaya\";}}'),('6a31908acfc8941c6c729ca5dc55d5a1fd4f4d2f','200.53.22.242','',1578451717,'__ci_last_regenerate|i:1578451717;FBRLH_state|s:32:\"cb2dd1daab1599677c9c5e195d3ddad0\";'),('21866a62f3be5979b651ea3591f1a58096446e44','200.53.22.242','',1578451806,'__ci_last_regenerate|i:1578451742;'),('0c6d87ed94b537508adf6ff7bc22bdb4ce0d2873','200.53.22.242','',1578452733,'__ci_last_regenerate|i:1578452733;sessao_user|a:1:{i:0;a:7:{s:2:\"id\";s:1:\"2\";s:4:\"nome\";s:6:\"Teste \";s:5:\"email\";s:15:\"teste@teste.com\";s:5:\"login\";s:5:\"teste\";s:5:\"senha\";s:32:\"202cb962ac59075b964b07152d234b70\";s:6:\"inicio\";s:10:\"1578452548\";s:6:\"imagem\";s:106:\"https://cdn0.iconfinder.com/data/icons/avatar-25/64/avatar-guy-handsome-man-hipster-beard-undercut-512.png\";}}'),('7060c8bbbb414525835d854278bb14cacf1a702d','200.53.22.242','',1578453412,'__ci_last_regenerate|i:1578453412;FBRLH_state|s:32:\"3dd30d7d50050736fedb1d69fe0c3c7f\";'),('d88568c0a0f547484a305c40b1fca4f72179311c','200.53.22.242','',1578453772,'__ci_last_regenerate|i:1578453772;sessao_user|a:7:{s:2:\"id\";s:1:\"2\";s:4:\"nome\";s:6:\"Teste \";s:5:\"email\";s:15:\"teste@teste.com\";s:5:\"login\";s:5:\"teste\";s:5:\"senha\";s:32:\"202cb962ac59075b964b07152d234b70\";s:6:\"inicio\";s:10:\"1578452838\";s:6:\"imagem\";s:106:\"https://cdn0.iconfinder.com/data/icons/avatar-25/64/avatar-guy-handsome-man-hipster-beard-undercut-512.png\";}'),('h5a7vdfvks8mup0glrdl5iogasbqqq27','::1','',1578453171,'__ci_last_regenerate|i:1578452619;FBRLH_state|s:32:\"37eee4bc5f1f4958b38bea197af104b8\";face_access_token|s:183:\"EAAKm1AHvs8UBANGMoLJAmZAmQihZBEQPXdwVeFEIZAaHtqFhOPqaL4epOWVZBQb1eUBmZBOdlVl7MfC4ZCSZBSYdFayy27izzOGPlqWZAANJZCJ5vbJqa6Y0lnmZB5UpPWDCTTkkJfNrroqshp4rNSZCqv4kmHKGpn6nacqohVpDZAE7kQZDZD\";sessao_user|a:7:{s:2:\"id\";s:1:\"1\";s:4:\"nome\";s:17:\"Wellisson Ribeiro\";s:5:\"email\";s:18:\"welleh10@gmail.com\";s:5:\"login\";s:9:\"wellisson\";s:5:\"senha\";s:32:\"e803adae1f5acc155699ad43e9b77629\";s:6:\"inicio\";s:10:\"1578452668\";s:6:\"imagem\";s:135:\"https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2640547489397903&height=50&width=50&ext=1581043405&hash=AeQq-hT7NG1Ykaya\";}'),('limblhn9emvp3miaqfgf1642unde07s7','::1','',1578453778,'__ci_last_regenerate|i:1578453309;'),('eb006c70ddb2c8e935f882b728ed59e4528b4cd8','200.53.22.242','',1578453772,'__ci_last_regenerate|i:1578453772;'),('14ff2c714f0559f4260425462b83df9661780ae5','200.53.22.242','',1578453862,'__ci_last_regenerate|i:1578453412;FBRLH_state|s:32:\"3dd30d7d50050736fedb1d69fe0c3c7f\";');
 
 /*Table structure for table `mensagens` */
 
@@ -42,33 +41,34 @@ CREATE TABLE `mensagens` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_de` int(11) NOT NULL,
   `id_para` int(11) NOT NULL,
-  `nick` varchar(100) DEFAULT '',
-  `mensagem` varchar(255) DEFAULT '',
-  `ip` varchar(50) DEFAULT '',
+  `mensagem` varchar(255) COLLATE utf8_unicode_ci DEFAULT '',
+  `ip` varchar(50) COLLATE utf8_unicode_ci DEFAULT '',
   `data_hora` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `mensagens` */
 
-insert  into `mensagens`(`id`,`id_de`,`id_para`,`nick`,`mensagem`,`ip`,`data_hora`) values (2,1,1,'Wellisson Ribeiro','teste','::1','2019-07-25 11:02:10'),(3,2,2,'Hamon','fsdfdf','::1','2019-07-25 11:02:31'),(4,2,2,'Hamon','dfsdfsdf','::1','2019-07-25 11:49:09'),(5,2,2,'Hamon','dsfsdfsdf','::1','2019-07-25 11:50:20'),(6,1,2,'','sdfsd','192.168.0.30','2019-08-16 16:08:56'),(7,2,0,'',NULL,'192.168.0.212','2019-08-16 16:08:58'),(8,2,1,'','Eae man','192.168.0.212','2019-08-16 16:08:58'),(9,1,2,'','funcionando','192.168.0.30','2019-08-16 16:08:58'),(10,2,1,'','deu boas','192.168.0.212','2019-08-16 16:08:58'),(11,1,2,'','049054','192.168.0.30','2019-08-16 16:08:59'),(12,1,2,'','sdf','192.168.0.30','2019-08-16 17:08:00'),(13,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(14,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(15,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(16,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(17,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(18,1,2,NULL,'teste','::1','2020-01-03 19:01:06'),(19,1,2,NULL,'sdfsdf','::1','2020-01-03 19:01:07'),(20,1,2,NULL,'teste','::1','2020-01-03 19:01:08'),(21,1,2,NULL,'sdfsd','::1','2020-01-03 19:01:08'),(22,1,2,NULL,'sdfsdf','::1','2020-01-03 19:01:08'),(23,1,2,NULL,'tesdsf','::1','2020-01-03 16:01:09'),(24,1,2,NULL,'sdfosdkf','::1','2020-01-03 16:01:11'),(25,1,2,NULL,'teste','::1','2020-01-03 16:01:14'),(26,1,2,NULL,'sdfsdf','::1','2020-01-03 16:01:19'),(27,1,2,NULL,'KAPDFKPOASDKFPPASDPOKDFSAPKOFDSAPOKFASDPOKFDAPFDPFKOASPSDOFKAPSDOFKPOASDKFPOAKSDFOPAKSDF','::1','2020-01-03 16:01:19'),(28,1,2,NULL,'dfsdf','::1','2020-01-03 18:01:30'),(29,1,2,NULL,'Ola','::1','2020-01-03 18:01:30'),(30,1,2,NULL,'sdfsdf','::1','2020-01-03 18:01:31'),(31,1,2,NULL,'sdff','::1','2020-01-03 18:01:31'),(32,1,2,NULL,'dfdf','::1','2020-01-03 18:01:31'),(33,1,2,NULL,'df','::1','2020-01-03 18:01:31'),(34,1,2,NULL,'df','::1','2020-01-03 18:01:31'),(35,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(36,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(37,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(38,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(39,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(40,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(41,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(42,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(43,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(44,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(45,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(46,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(47,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(48,1,2,NULL,'d','::1','2020-01-03 18:01:31'),(49,1,2,NULL,'fasd','::1','2020-01-03 18:01:31'),(50,1,2,NULL,'fsd','::1','2020-01-03 18:01:31'),(51,1,2,NULL,'f','::1','2020-01-03 18:01:31'),(52,1,2,NULL,'sdf','::1','2020-01-03 18:01:31'),(53,1,3,NULL,'sdfsdf','::1','2020-01-03 18:01:34');
+insert  into `mensagens`(`id`,`id_de`,`id_para`,`mensagem`,`ip`,`data_hora`) values (1,1,2,'ola','200.53.22.242','2020-01-07 23:01:53'),(2,2,1,'teste','200.53.22.242','2020-01-07 23:01:53'),(3,2,1,'teste','200.53.22.242','2020-01-07 23:01:54'),(4,1,2,'sd','200.53.22.242','2020-01-07 23:01:55'),(5,1,2,'s','200.53.22.242','2020-01-07 23:01:55'),(6,1,2,'s','200.53.22.242','2020-01-07 23:01:55'),(7,1,2,'s','200.53.22.242','2020-01-07 23:01:55'),(8,1,2,'s','200.53.22.242','2020-01-07 23:01:55'),(9,1,2,'s','200.53.22.242','2020-01-07 23:01:55'),(10,2,1,'t','200.53.22.242','2020-01-07 23:01:55'),(11,2,1,'t','200.53.22.242','2020-01-07 23:01:55'),(12,2,1,'sdf','::1','2020-01-08 01:01:17'),(13,2,1,'t','::1','2020-01-08 01:01:18');
 
 /*Table structure for table `usuarios` */
 
 DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(220) NOT NULL,
-  `email` varchar(220) NOT NULL,
-  `login` varchar(50) NOT NULL,
-  `senha` varchar(50) NOT NULL,
-  `inicio` int(11) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(220) COLLATE utf8_unicode_ci NOT NULL,
+  `login` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `senha` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `inicio` int(11) DEFAULT '0',
+  `imagem` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 /*Data for the table `usuarios` */
 
-insert  into `usuarios`(`id`,`nome`,`email`,`login`,`senha`,`inicio`) values (1,'Wellisson Ribeiro','welleh10@gmail.com','wellisson','e803adae1f5acc155699ad43e9b77629',1578072055),(2,'Contato Padrao','contato@contato.com','contato','e8d95a51f3af4a3b134bf6bb680a213a',0),(3,'Teste','teste@teste.com','teste','askdfadf',0),(4,'Joao','spdkfpods@sdokfd.com','oksdfokdfs','asdfsdf',0),(5,'Pedro','osdkfods@teste.com','osdkfods','oksdfods',0),(6,'Maria','ksdfo.com.tes','sdfsdf','sdfdsf',0),(7,'Lucas','234342.com.br','sdfsdf','we23423',0),(8,'Madalena','ksdofk.com.br','oskdfos','2o342423',0);
+insert  into `usuarios`(`id`,`nome`,`email`,`login`,`senha`,`inicio`,`imagem`) values (1,'Wellisson Ribeiro','welleh10@gmail.com','wellisson','e803adae1f5acc155699ad43e9b77629',1578453861,'https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=2640547489397903&height=50&width=50&ext=1581043405&hash=AeQq-hT7NG1Ykaya'),(2,'Teste ','teste@teste.com','teste','202cb962ac59075b964b07152d234b70',1578453772,'https://cdn0.iconfinder.com/data/icons/avatar-25/64/avatar-guy-handsome-man-hipster-beard-undercut-512.png');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
